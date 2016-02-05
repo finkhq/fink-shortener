51859f5bc85ff1ac07bd34b0458db7e60fb6c161
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-02-05 11:18:13 +0100

    Add a way to validate an url

--- index.js
+++ index.js
@@ -3,9 +3,20 @@
 const ALPHABET_BASE58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 const base58 = require('base-x')(ALPHABET_BASE58)
 const emoji = require('base64-emoji')
+const parseURI = require('parse-uri')
 
-module.exports = function (id) {
+function shorten (id) {
   let hash = base58.encode(id.toString())
   let hashEmoji = emoji.encode(id.toString()).toString()
   return {id, hash, hashEmoji}
 }
+
+function isValid (hostname, something, opts) {
+  const URI = parseURI(something, opts)
+  if (!URI.protocol) return false
+  if (hostname === URI.host) return false
+  return true
+}
+
+module.exports = shorten
+module.exports.isValid = isValid
--- test/index.js
+++ test/index.js
@@ -7,10 +7,38 @@ require('should')
 const shortener = require('..')
 
 describe('fink shortener ::', function () {
-  it('create a hash from a number', function () {
-    const short = shortener(123)
-    short.id.should.be.an.Number()
-    short.hash.should.be.a.String()
-    short.hashEmoji.should.be.a.String()
+  describe('.shorten', function () {
+    it('create a hash from a number', function () {
+      const short = shortener(123)
+      short.id.should.be.an.Number()
+      short.hash.should.be.a.String()
+      short.hashEmoji.should.be.a.String()
+    })
+  })
+
+  describe('.isValid', function () {
+    describe('valid', function () {
+      it('when the URI is from different hostname', function () {
+        const isValid = shortener.isValid('myhost.com', 'http://kikobeats.com')
+        isValid.should.be.true()
+      })
+
+      it('from bittorrent protocol', function () {
+        const isValid = shortener.isValid('myhost.com', 'magnet:?xt=urn:sha1:PDAQRAOQQRYS76MRZJ33LK4MMVZBDSCL')
+        isValid.should.be.true()
+      })
+    })
+
+    describe('invalid', function () {
+      it('when the URI is from the same hostname', function () {
+        const isValid = shortener.isValid('google.com', 'https://google.com/')
+        isValid.should.be.false()
+      })
+
+      it('when is not a valid URI', function () {
+        const isValid = shortener.isValid('google.com', 'aoskdpoakd')
+        isValid.should.be.false()
+      })
+    })
   })
 })
