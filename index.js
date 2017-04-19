7db75faf6f2de34f07ad49c05eb239feb8fb728b
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-04-19 18:16:29 +0200

    update base58 dependency

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const base58 = require('encode-base58')
 
 module.exports = function shorten (id) {
   const hash = base58(id)
-  const hashEmoji = base58.emojis(id)
+  const hashEmoji = base58.emoji(id)
   return { id, hash, hashEmoji }
 }
